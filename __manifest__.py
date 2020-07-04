f8ccd44cdff9c9d04ea0390a1e12cadaa4aabf9e
Lyrad <lyradb@gmail.com>
2020-07-05 07:06:13 +0800

    -- first commit

--- __manifest__.py
+++ __manifest__.py
@@ -27,8 +27,16 @@
         'security/security.xml',
         'views/views.xml',
         'data/data.xml',
-        # 'views/template
-        # s.xml',
+        'reports/templates/paper_sizes.xml',
+        'reports/templates/entry_forms_template.xml',
+        'reports/templates/refferal_forms_template.xml',
+        'reports/templates/item_stock_card_template.xml',
+        'reports/templates/medicines_inventory_template.xml',
+        'reports/templates/medical_record_template.xml',
+        'reports/templates/daily_billout_template.xml',
+        'reports/templates/lab_results_template.xml',
+        'reports/templates/prescription_template.xml',
+        'reports/templates/medical_certificate_template.xml',
     ],
     
     # 'qweb': ['static/src/xml/many2many_checkboxes.xml', ],
